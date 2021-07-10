3edcad34979221628ea93b7c9286966c906e2993
Raul Torres <jr.torsa@gmail.com>
2021-07-09 22:55:03 -0600

    haciendo el ejercicio

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    increment: 0
+  }
+  
   render() {
+    const { increment } = this.state
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{increment}</span>
+        <button id="inc" onClick={() => this.setState({increment: increment + 1})}>Incrementa</button>
       </div>
     );
   }
